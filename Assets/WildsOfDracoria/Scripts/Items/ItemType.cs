2800e1e25aa7d20e164420084dc661f5b46607f3
Corinhothekid <109396377+Corinhothekid@users.noreply.github.com>
2026-07-04 18:41:48 -0400

    Add item type enum

---
+++ Assets/WildsOfDracoria/Scripts/Items/ItemType.cs
@@ -0,0 +1,19 @@
+namespace WildsOfDracoria.Items
+{
+    public enum ItemType
+    {
+        Fish,
+        Ore,
+        Wood,
+        Food,
+        Weapon,
+        Armor,
+        Tool,
+        Material,
+        MonsterDrop,
+        TradeGood,
+        QuestItem,
+        ShipPart,
+        Misc
+    }
+}
