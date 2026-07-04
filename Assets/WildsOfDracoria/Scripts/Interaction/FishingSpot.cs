5f2dee464c7a47b210e52b69d36466d860f92d34
Corinhothekid <109396377+Corinhothekid@users.noreply.github.com>
2026-07-04 17:51:07 -0400

    Add fishing prototype interaction

---
+++ Assets/WildsOfDracoria/Scripts/Interaction/FishingSpot.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using UnityEngine;
+using WildsOfDracoria.Player;
+using WildsOfDracoria.Systems;
+
+namespace WildsOfDracoria.Interaction
+{
+    public class FishingSpot : InteractableBase
+    {
+        [SerializeField] private float catchTimeSeconds = 3f;
+        [SerializeField] private int fishingXPReward = 25;
+
+        private readonly string[] catchTable =
+        {
+            "Small Silverfin",
+            "River Trout",
+            "Old Boot",
+            "Rare Golden Carp"
+        };
+
+        private bool isFishing;
+
+        public override void Interact(PlayerInteractor interactor)
+        {
+            if (isFishing)
+            {
+                return;
+            }
+
+            StartCoroutine(FishRoutine());
+        }
+
+        private IEnumerator FishRoutine()
+        {
+            isFishing = true;
+            GameManager.Instance.DialogueUI?.ShowLine("You cast your line into the harbor water...");
+
+            yield return new WaitForSeconds(catchTimeSeconds);
+
+            var caughtItem = catchTable[Random.Range(0, catchTable.Length)];
+            GameManager.Instance.AddItem(caughtItem);
+            GameManager.Instance.GainSkillXP("Fishing", fishingXPReward);
+            GameManager.Instance.DialogueUI?.ShowLine($"You caught: {caughtItem}");
+
+            isFishing = false;
+        }
+    }
+}
