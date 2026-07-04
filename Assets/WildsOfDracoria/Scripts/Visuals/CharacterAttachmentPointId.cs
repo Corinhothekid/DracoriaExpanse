374c66cda3487d17eeb3928948ec15da72515851
Corinhothekid <109396377+Corinhothekid@users.noreply.github.com>
2026-07-04 19:20:50 -0400

    Add character attachment point ids

---
+++ Assets/WildsOfDracoria/Scripts/Visuals/CharacterAttachmentPointId.cs
@@ -0,0 +1,18 @@
+namespace WildsOfDracoria.Visuals
+{
+    public enum CharacterAttachmentPointId
+    {
+        Head,
+        Hair,
+        Face,
+        Chest,
+        Back,
+        Shoulders,
+        Hands,
+        Belt,
+        Legs,
+        Feet,
+        MainHand,
+        OffHand
+    }
+}
