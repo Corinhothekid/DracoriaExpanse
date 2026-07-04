410632634998ba3f0ab6be0e858ce18cb76599c1
Corinhothekid <109396377+Corinhothekid@users.noreply.github.com>
2026-07-04 17:51:48 -0400

    Add dialogue UI

---
+++ Assets/WildsOfDracoria/Scripts/UI/DialogueUI.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using UnityEngine.UI;
+using WildsOfDracoria.Systems;
+
+namespace WildsOfDracoria.UI
+{
+    public class DialogueUI : MonoBehaviour
+    {
+        [SerializeField] private GameObject panel;
+        [SerializeField] private Text dialogueText;
+        [SerializeField] private float autoHideSeconds = 5f;
+
+        private float hideAtTime;
+
+        private void Awake()
+        {
+            GameManager.Instance?.RegisterDialogueUI(this);
+            Hide();
+        }
+
+        private void Update()
+        {
+            if (panel != null && panel.activeSelf && Time.time >= hideAtTime)
+            {
+                Hide();
+            }
+        }
+
+        public void ShowLine(string line)
+        {
+            if (dialogueText != null)
+            {
+                dialogueText.text = line;
+            }
+
+            if (panel != null)
+            {
+                panel.SetActive(true);
+            }
+
+            hideAtTime = Time.time + autoHideSeconds;
+        }
+
+        public void Hide()
+        {
+            if (panel != null)
+            {
+                panel.SetActive(false);
+            }
+        }
+    }
+}
