92bb6e4b3309cfb5d1c99a81e6bfa9396f2da223
Corinhothekid <109396377+Corinhothekid@users.noreply.github.com>
2026-07-04 22:16:39 -0400

    Add contract status enum

---
+++ Assets/WildsOfDracoria/Scripts/Contracts/ContractStatus.cs
@@ -0,0 +1,12 @@
+namespace WildsOfDracoria.Contracts
+{
+    public enum ContractStatus
+    {
+        Available,
+        Accepted,
+        ReadyToTurnIn,
+        Completed,
+        Failed,
+        Abandoned
+    }
+}
