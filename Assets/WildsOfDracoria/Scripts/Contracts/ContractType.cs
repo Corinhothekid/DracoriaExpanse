e4e6cc7b2f894335cf7ef348839a982b5d1cf41c
Corinhothekid <109396377+Corinhothekid@users.noreply.github.com>
2026-07-04 22:16:34 -0400

    Add contract type enum

---
+++ Assets/WildsOfDracoria/Scripts/Contracts/ContractType.cs
@@ -0,0 +1,15 @@
+namespace WildsOfDracoria.Contracts
+{
+    public enum ContractType
+    {
+        Fishing,
+        Gathering,
+        Combat,
+        Delivery,
+        Crafting,
+        Local,
+        Business,
+        Guild,
+        Kingdom
+    }
+}
